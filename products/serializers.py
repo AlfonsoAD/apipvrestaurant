d793117d52a3097da40acc4f2385251b36b20600
AlfonsoAD <alfonsoad097@gmail.com>
2023-11-26 16:51:50 -0600

    Cambios products

--- products/serializers.py
+++ products/serializers.py
@@ -1,13 +1,14 @@
 from rest_framework import serializers
 from .models import Product
+from categorys.models import Category
 from categorys.serializers import CategorySerializer
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    category = CategorySerializer()
+    category = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all().filter(is_active=True))
 
     class Meta:
         model = Product
         fields = ['id', 'name', 'description', 'price',
                   'image', 'is_active', 'created_at', 'category']
-        read_only_fields = ('id', 'created_at')
