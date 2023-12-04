60af31ae051a23a46e1ba631822bef041e085870
AlfonsoAD <alfonsoad097@gmail.com>
2023-12-03 23:15:32 -0600

    Menus details menus

--- menus/serializers.py
+++ menus/serializers.py
@@ -5,5 +5,5 @@ from .models import Menu
 class MenuSerializer(serializers.ModelSerializer):
     class Meta:
         model = Menu
-        fields = ["id", "name", "description", "is_active",]
+        fields = ["id", "name", "description", "is_active", "created_at"]
         read_only_fields = ("id", "created_at")
